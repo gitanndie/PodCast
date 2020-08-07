bd26fbabd8fedf65ccb52351777667746c01ad95
Anndie Gonzalez <anndiegonzalez@gmail.com>
2020-08-07 16:55:49 -0500

    Subiendo Repo

--- js/main.js
+++ js/main.js
@@ -1,11 +1,43 @@
-window.addEventListener("load", function () {
-    document.getElementById("play1").addEventListener("click", sonar);
+const audio = document.querySelector("#audio");
+const imagenes = document.querySelector(".spoty-image");
+
+const audio1 = document.querySelector("#audio1");
+const imagenes1 = document.querySelector(".spoty-image1");
+
+const audio2 = document.querySelector("#audio2");
+const imagenes2 = document.querySelector(".spoty-image2");
+
+const audio3 = document.querySelector("#audio3");
+const imagenes3 = document.querySelector(".spoty-image3"); 
+
+imagenes.addEventListener("click", function() {
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
 });
 
-function sonar() {
-    var sonido = document.createElement("audio");
-    sonido.setAttribute("src", "sonidos/extintor.mp3");
-    document.body.appendChild(sonido);
-    document.getElementById("play1").removeEventListener("click", sonar);
-}
+imagenes1.addEventListener("click", function() {
+    if (audio1.paused) {
+        audio1.play();
+    } else {
+        audio1.pause();
+    }
+});
 
+imagenes2.addEventListener("click", function() {
+    if (audio2.paused) {
+        audio2.play();
+    } else {
+        audio2.pause();
+    }
+});
+
+imagenes3.addEventListener("click", function() {
+    if (audio3.paused) {
+        audio3.play();
+    } else {
+        audio3.pause();
+    }
+}); 
